8dfc04eab287e1467077898ea3948c8376f73891
Francy Lang <francy.lang@gmail.com>
2017-07-18 17:40:29 -0600

    constructor function and array

--- script.js
+++ script.js
@@ -1,47 +1,66 @@
-function addCards() {
-  var titleInput = document.querySelector(".title-input").value
-  var bodyInput = document.querySelector(".body-input").value
-  $('.idea-card-parent').prepend(`<article class="idea-card">
-    <h2>${titleInput}</h2>
-    <input class="delete-btn" type="image" src="FEE-ideabox-icon-assets/delete.svg" />
-    <p class="body-text">${bodyInput}</p>
+function CardElements(title, body) {
+  this.title = title;
+  this.body = body;
+  this.id = Date.now();
+  this.quality = 'Swill';
+}
+
+// create empty array []
+var cardArray = []
+
+function addCards (card) {
+  $('.idea-card-parent').prepend(
+  `<article class="idea-card" id="${this.id}">
+      <h2>${this.title}</h2>
+        <input class="delete-btn" type="image" src="FEE-ideabox-icon-assets/delete.svg" />
+        <p class="body-text">${this.body}</p>
     <div class="ratings">
-    <input class="upvote-btn" type="image" src="FEE-ideabox-icon-assets/upvote.svg" />
-    <input class="downvote-btn" type="image" src="FEE-ideabox-icon-assets/downvote.svg" />
-    <p class="quality">quality:<span class="qualityRating">swill</span></p>
+      <input class="upvote-btn" type="image" src="FEE-ideabox-icon-assets/upvote.svg" />
+      <input class="downvote-btn" type="image" src="FEE-ideabox-icon-assets/downvote.svg" />
+      <p class="quality">quality:<span class="qualityRating">swill</span></p>
     <hr>
     </div>
-    </article>`)
+    </article>`);
+}
+
+function storeCards() {
+  localStorage.setItem("array", JSON.stringify(cardArray));
 }
 
 function fireCards (event) {
-  event.preventDefault ()
-  addCards()
+  event.preventDefault ();
+  var newCard = new CardElements ($('.title-input').val(), $('.body-input').val());
+  console.log(newCard);
+  cardArray.push(newCard)
+  addCards(newCard);
+  console.log(cardArray);
+  storeCards();
 }
+
 
 function upVote() {
-  var spanText = $('.qualityRating').text();
-  if (spanText === 'swill') {
+  var currentQuality = this.quality;
+  if (currentQuality === 'swill') {
   $('.qualityRating').text('plausible')
-} else if (spanText === 'plausible') {
+} else if (currentQuality === 'plausible') {
   $('.qualityRating').text('genius')
 } else {
   $('.qualityRating').text('genius')
  }
-}
+};
 
-function downVote() {
-  var spanText = $('.qualityRating').text();
-  if (spanText === 'swill') {
+function downVote () {
+  var currentQuality = this.quality;
+  if (currentQuality === 'swill') {
   $('.qualityRating').text('swill')
-} else if (spanText === 'plausible') {
+} else if (currentQuality === 'plausible') {
   $('.qualityRating').text('swill')
-} else if (spanText === 'genius') {
+} else if (currentQuality === 'genius') {
   $('.qualityRating').text('plausible')
 } else {
   $('.qualityRating').text('swill')
  }
-}
+};
 
 
 function upvoteListener(event) {
