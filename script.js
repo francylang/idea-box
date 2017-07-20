9294a1aa7dd6c36972b12701805ea4fbd2e333ac
Francy Lang <francy.lang@gmail.com>
2017-07-20 10:14:42 -0600

    final changes made to html and js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+var cardArray = []
+var cardList = $('.idea-card-parent')
+
 function CardElements(title, body) {
   this.title = title;
   this.body = body;
@@ -5,197 +8,152 @@ function CardElements(title, body) {
   this.quality = 'Swill';
 }
 
-
-$(window).on('load', function(){
+$(window).on('load', function() {
   console.log('working!!!');
   retrieveLocalStorage();
   clearInputs();
 })
 
-// create empty array []
-var cardArray = []
-
-function addCards (buildCard) {
+function addCards(buildCard) {
   $('.idea-card-parent').prepend(
-`<article class="idea-card" id="${buildCard.id}">
-  <h2 contenteditable="true">${buildCard.title}</h2>
-
-
-<div id="delete">
-</div>
-    <p class="body-text" contenteditable="true">${buildCard.body}</p>
-  <div class="ratings">
-<div id="upvote">
-</div>
-
-<div id="downvote"
-</div>
-    <p class="quality">quality: <span class="qualityRating">${buildCard.quality}</span></p>
-    <hr>
-  </div>
-</article>`);
+    `<article class="idea-card" id="${buildCard.id}">
+      <h2 contenteditable="true">${buildCard.title}</h2>
+      <div class="delete-btn" id="delete">
+      </div>
+      <p class="body-text" contenteditable="true">${buildCard.body}</p>
+      <div class="ratings">
+      <div class="upvote-btn" id="upvote"></div>
+      <div class="downvote-btn" id="downvote"></div>
+        <p class="quality">quality: <span class="${buildCard.id}">${buildCard.quality}</span></p>
+      </div>
+      <hr>
+    </article>`);
 }
 
-
-function fireCards () {
-  var newCard = new CardElements ($('.title-input').val(), $('.body-input').val());
-  // console.log(newCard);
+function fireCards() {
+  var newCard = new CardElements($('.title-input').val(), $('.body-input').val());
   cardArray.push(newCard)
   addCards(newCard);
-  console.log(cardArray);
   storeCards();
   clearInputs();
 }
 
-// climb and find id
-// pull the quality value from the index that that value lives on in the array
-// function upVote() {
-//   var newQuality;
-//   cardArray.forEach(function (card) {
-//   if (card.id == currentQuality) {
-//     if (card.quality === 'Swill') {
-//       card.quality === 'Plausible'
-//       newQuality === card.quality;
-//   } else if (card.quality === 'Plausible') {
-//       card.quality === 'Genius'
-//       newQuality === card.quality;
-//   } else {
-//     card.quality === 'Genius'
-// }}
-//   })
-// };
-
-function downVote () {
-  var currentQuality = this.quality;
-  if (currentQuality === 'swill') {
-  $('.qualityRating').text('swill')
-} else if (currentQuality === 'plausible') {
-  $('.qualityRating').text('swill')
-} else if (currentQuality === 'genius') {
-  $('.qualityRating').text('plausible')
-} else {
-  $('.qualityRating').text('swill')
- }
-};
-
-
-$('.idea-card-parent').on('click', '#delete', function(){
+$('.idea-card-parent').on('click', '#delete', function() {
   var currentCardId = $(this).closest('.idea-card')[0].id
-cardArray.forEach(function (card, index){
-if (currentCardId == card.id){
-  cardArray.splice(index, 1)
-}
-})
-storeCards()
-$(this).parents('.idea-card').remove()
+  cardArray.forEach(function(card, index) {
+    if (currentCardId == card.id) {
+      cardArray.splice(index, 1)
+    }
+  })
+  storeCards()
+  $(this).parents('.idea-card').remove()
 })
 
-$('.idea-card-parent').on('click', '#upvote', function (event){
+$('.idea-card-parent').on('click', '#upvote', function(event) {
+  event.preventDefault();
+  var cardId = $(this).closest('.idea-card')[0].id
+  cardArray.forEach(function(card) {
+    if (card.id == cardId) {
+      if (card.quality === "Swill") {
+        card.quality = "Plausible";
+        $('.' + cardId).text('Plausible')
+      } else if (card.quality === "Plausible") {
+        card.quality = "Genius"
+        $('.' + cardId).text('Genius')
+      } else {
+        card.quality = "Genius"
+        $('.' + cardId).text('Genius')
+      }
+    }
+    storeCards();
+  })
+});
+
+$('.idea-card-parent').on('click', '#downvote', function (event){
   event.preventDefault();
   var cardId = $(this).closest('.idea-card')[0].id
   console.log(cardId)
-  var newQuality;
   cardArray.forEach(function (card) {
+    console.log(cardId)
   if (card.id == cardId) {
-    if (card.quality === 'Swill') {
-      card.quality === 'Plausible'
-      newQuality === card.quality;
-  } else if (card.quality === 'Plausible') {
-      card.quality === 'Genius'
-      newQuality === card.quality;
-  } else {
-    card.quality === 'Genius'
-}}
-  })
-});
-
-// $('.idea-card-parent').on('click', '#downvote', downvoteListener)
-
-
-$('.idea-card-parent').on('click', '#upvote', function() {
-  var cardId = $(this).closest('idea-card')[0].id
-  console.log(CardId)
-  var newQuality;
-  cardArray.forEach(function (card, index){
-    if (card.id == cardId) {
-      if (card.quality === 'Swill') {
-        card.quality === 'Plausible'
-        newQuality === card.quality;
+    if (card.quality === 'Genius') {
+        card.quality = 'Plausible';
+        $('.' + cardId).text('Plausible')
       } else if (card.quality === 'Plausible') {
-        card.quality === 'Genius'
-        newQuality === 'Genius'
+        card.quality = 'Swill'
+        $('.' + cardId).text('Swill')
+      }else{
+        card.quality = 'Swill'
+        $('.' + cardId).text('Swill')
       }
-    }
-  })
+  }
+  storeCards();
+})
 });
 
 function storeCards() {
-  console.log("before storing card: ", cardArray)
   localStorage.setItem('array', JSON.stringify(cardArray));
-  console.log("after storing card: ", cardArray)
   clearInputs()
-
 };
-// clear input fields after prepend
+
 function clearInputs() {
   $('.title-input').val('');
   $('.body-input').val('');
   $('title-input').focus();
 }
 
-
-
-
-// retrieve card from local storage
-// WE WANT THIS TO FIRE ON PAGE LOAD?
 function retrieveLocalStorage() {
-  cardArray = JSON.parse(localStorage.getItem('array')) || [] ;
+  cardArray = JSON.parse(localStorage.getItem('array')) || [];
   cardArray.forEach(function(card) {
-    addCards (card);
+    addCards(card);
   })
 }
 
-  $('.search-input').on('keyup',searchCards)
+$('.save-btn').on('click', function(event) {
+  event.preventDefault();
+  fireCards()
+})
 
-// search
+cardList.on('keyup', 'h2', function(event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    this.blur();
+  }
+  var id = $(this).closest('.idea-card')[0].id;
+  var title = $(this).text();
+  cardArray.forEach(function(card) {
+    if (card.id == id) {
+      card.title = title;
+    }
+  })
+  storeCards();
+})
+
+cardList.on('keyup', '.body-text', function(event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    this.blur();
+  }
+  var id = $(this).closest('.idea-card')[0].id;
+  var body = $(this).text();
+  cardArray.forEach(function(card) {
+    if (card.id == id) {
+      card.body = body;
+    }
+  })
+  storeCards();
+})
+
+$('.search-input').on('keyup', searchCards)
 function searchCards() {
   var search = $(this).val().toUpperCase();
-  var results = cardArray.filter(function(newCard) {
-    return newCard.title.toUpperCase().includes(search) || newCard.body.toUpperCase().includes(search) ||
-    newCard.quality.toUpperCase().includes(search);
-    });
-  $('.search-input').empty();
- for (var i = 0; i < results.length; i++) {
-   addCard(results[i]);
+  var results = cardArray.filter(function(elementCard) {
+    return elementCard.title.toUpperCase().includes(search) ||
+           elementCard.body.toUpperCase().includes(search) ||
+           elementCard.quality.toUpperCase().includes(search);
+  });
+  $('.idea-card-parent').empty();
+  for (var i = 0; i < results.length; i++) {
+    addCards(results[i]);
   }
 }
-
-
-
-// if (event.keyCode === 13) {
-//   event.preventDefault();
-//   this.blur();}
-
-
-
-//
-// $('.idea-card-parent').empty();
-// results.forEach(function(result) {
-//   addCard(result)
-// });
-
-// EVENT LISTENER FOR SEARCH
-$('.save-btn').on('click', function(event){
-  event.preventDefault ();
-  fireCards ()
-})
-// $('.idea-card-parent').on('click', '.downvote-btn', downvoteListener)
-
-var id = $(this).closest('.idea-card')[0].id;
-var edit = this.text;
-cardArray.forEach(function(card) {
-  if (card.id == id) {
-    card.id = edit;
-
-  }
-})
